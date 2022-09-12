0949b5b5e913b00fd15bd1dbcc2d22a92f5fd957
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-09-12 18:10:58 -0300

    requisito 03

--- src/insights.py
+++ src/insights.py
@@ -31,21 +31,18 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
+    lista = read(path)
+    result = {}
+    for item in lista:
+        industry = item["industry"]
+        if industry != "":
+            if industry not in result and industry:
+                result[industry] = {"jobs": 0}
+            result[industry]["jobs"] += 1
+    return result
 
-    Must call `read`
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+# get_unique_industries("src/jobs.csv")
 
 
 def filter_by_industry(jobs, industry):
