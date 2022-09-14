ba67fec07cbafd66a811eec46d26ac0f2e895a29
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-09-13 21:26:51 -0300

    remove arquivos

--- tests/counter/test_counter.py
+++
@@ -1,8 +0,0 @@
-from src.counter import count_ocurrences
-
-
-def test_counter():
-    assert count_ocurrences("src/jobs.csv", "Python") == 1639
-    assert count_ocurrences("src/jobs.csv", "python") == 1639
-    assert count_ocurrences("src/jobs.csv", "Javascript") == 122
-    assert count_ocurrences("src/jobs.csv", "javascript") == 122
--- tests/sorting/test_sorting.py
+++
@@ -1,9 +0,0 @@
-from src.jobs import read
-from src.sorting import sort_by
-
-jobs = read("src/jobs.csv")
-
-
-def test_sort_by_criteria():
-    sort_by(jobs, "min_salary")
-    assert jobs[0]["min_salary"] == "19857"
